55d6cdfa436e1a1f3f6e6fc0b6459e7d965fd681
Josh Taylor <taylor.joshua05@gmail.com>
2022-06-03 14:52:23 -0400

    first commit

---
+++ OAuth-Project/src/test/java/com/oauthproject/OAuthProject/OAuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauthproject.OAuthProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
